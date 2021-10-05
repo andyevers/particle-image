24d925f7a24c461632ed50534409ebcec2e4b858
andyevers <76075074+andyevers@users.noreply.github.com>
2021-10-05 12:29:09 -0500

    Update particle-image.js

--- particle-image.js
+++ particle-image.js
@@ -4,65 +4,94 @@
  * @property {Number} y (float) y coordinate on plane
  */
 
-function ParticleImage(settings) {
+function ParticleImage(settings = {}) {
     const _this = this
 
     // DEFAULTS
     //-----------------------------------------------
-
-    const {
-        output = null,
-        width = 500,
-        height = 500,
-        paddingX = 100,
-        paddingY = 100,
-        density = 5,
-        frames = 100,
-        particleShuffle = true, // randomizes order in which particles are given the transition properties provided
-        particleProperties = {}, // default set below
-        animationMoveFunction = 'linear',
-        animationTimingFunction = 'easeOut',
-        animationPropertyFunction = 'linear'
-    } = settings
-
-    if (width - paddingX <= 0) throw console.error('your paddingX must be less than the width')
-    if (height - paddingY <= 0) throw console.error('your paddingY must be less than the height')
-
-    this.output = output
-    this.width = width
-    this.height = height
-    this.paddingX = paddingX
-    this.paddingY = paddingY
-    this.density = density
-    this.frames = frames
-    this.particleShuffle = particleShuffle
-    this.animationMoveFunction = animationMoveFunction
-    this.animationTimingFunction = animationTimingFunction
-    this.animationPropertyFunction = animationPropertyFunction
-    this.particleProperties = {
-        fill: getOrDefault(particleProperties.fill, '#000000'),
-        radius: getOrDefault(particleProperties.radius, 4),
-        opacity: getOrDefault(particleProperties.opacity, 1)
-    }
-
-    this.particles = []
-
-    this._requestedFrame = null // id of the last requested animation frame
-
-    this.canvas = {
-        element: null,
-        context: null,
-        clear: function () {
-            this.context.clearRect(0, 0, _this.width, _this.height)
+    const defaults = {
+        output: null,
+        image: {
+            width: 500,
+            height: 500,
+            alignH: 'center',
+            alignV: 'center',
+            contain: true,
         },
-        init: function () {
-            this.element = document.createElement('canvas')
-            this.element.width = _this.width
-            this.element.height = _this.height
-            this.context = this.element.getContext('2d')
-            if (_this.output) _this.output.appendChild(this.element)
+        canvas: {
+            element: null,
+            width: 500,
+            height: 500,
+            padding: {
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+            },
+        },
+        particles: {
+            density: 8,
+            shuffle: true,
+            array: [],
+            properties: {
+                fill: '#000000',
+                opacity: 1,
+                radius: 3
+            },
+        },
+        animation: {
+            frames: 50,
+            contain: true,
+            moveFunction: 'linear',
+            timingFunction: 'easeOut',
+            propertyFunction: 'linear',
         }
     }
+
+    let mergedSettings = mergeDeep(defaults, settings)
+
+    this.output = mergedSettings.output
+    this.image = mergedSettings.image
+    this.canvas = mergedSettings.canvas
+    this.particles = mergedSettings.particles
+    this.animation = mergedSettings.animation
+
+    //add canvas props/methods
+    this.canvas.context = null
+    this.canvas.clear = function () {
+        this.context.clearRect(0, 0, this.width, this.height)
+    }
+    this.canvas.init = function () {
+        this.element = this.element instanceof HTMLCanvasElement ? this.element : document.createElement('canvas')
+        this.element.width = this.width
+        this.element.height = this.height
+        this.context = this.element.getContext('2d')
+        if (_this.output) _this.output.appendChild(this.element)
+    }
+
+    //add image props / methods
+    this.image.getContainedDimensions = function () {
+        let { width, height } = this
+
+        if (this.contain === true) {
+            const padding = _this.canvas.padding
+            const maxWidth = _this.canvas.width - padding.left - padding.right
+            const maxHeight = _this.canvas.height - padding.top - padding.bottom
+
+            const adjustedWidthRatio = Math.min(width, maxWidth) / width
+            const adjustedHeightRatio = Math.min(height, maxHeight) / height
+            const minAdjustmentRatio = Math.min(adjustedWidthRatio, adjustedHeightRatio)
+
+            width *= minAdjustmentRatio
+            height *= minAdjustmentRatio
+        }
+
+        return { width: width, height: height }
+    }
+
+    this.canvas.init()
+
+    this._requestedFrame = null // id of the last requested animation frame
 
     this.cancelAnimation = function () {
         if (this._requestedFrame === null) return false
@@ -77,44 +106,46 @@ function ParticleImage(settings) {
         imageObj.onload = function (e) {
             const imageData = _this.getImageData(imageObj)
             let transitionDataArr = _this.getParticleDataFromImage(imageData, useImageColor)
-            _this.setParticleTransitions(transitionDataArr)
+
+            _this.setParticles(transitionDataArr)
             _this.animate()
-            if (typeof onload === 'function') onload(this.particles)
+            if (typeof onload === 'function') onload(_this.particles.array)
         }
         imageObj.src = src
     }
 
-    this.setParticleTransitions = function (transitionDataArr = []) {
+    this.setParticles = function (transitionDataArr = []) {
         //if new particles are required, add them until there is enough
         let newParticles = []
-        while (this.particles.length < transitionDataArr.length) {
+        while (this.particles.array.length < transitionDataArr.length) {
             let particle = new this.Particle()
-            this.particles.push(particle)
+            this.particles.array.push(particle)
             newParticles.push(particle)
         }
 
         // get particles required for the transition
-        let particlesToUse = transitionDataArr.length < this.particles.length
-            ? getEvenlyDistributedSample(this.particles, transitionDataArr.length)
-            : this.particles
+        let particlesToUse = transitionDataArr.length < this.particles.array.length
+            ? getEvenlyDistributedSample(this.particles.array, transitionDataArr.length)
+            : this.particles.array
 
         //shuffle transition data if this.particleShuffle = true
-        transitionDataArr = this.particleShuffle ? shuffleArray(transitionDataArr) : transitionDataArr
+        transitionDataArr = this.particles.shuffle ? shuffleArray(transitionDataArr) : transitionDataArr
 
         // add transition data to required particles, otherwise prepare fade out
         let curTransitionDataIndex = 0
-        this.particles.forEach(particle => {
+        this.particles.array.forEach(particle => {
+            //if particle is not new, set to transition from current position and props
             if (!newParticles.includes(particle)) {
                 particle.transitionData.fromProperties = particle.properties
                 particle.transitionData.fromPoint = particle.point
             }
             if (particlesToUse.includes(particle)) {
                 let transitionData = transitionDataArr[curTransitionDataIndex]
-                particle.updateTransitionData({ toProperties: _this.particleProperties })
+                particle.updateTransitionData({ toProperties: this.particles.properties })
                 particle.updateTransitionData(transitionData)
                 curTransitionDataIndex++
             } else {
-                let fadeOutPoint = this.getRandomPoint(20, particle.point)
+                let fadeOutPoint = this.getRandomPoint({ maxDistance: 20, fromPoint: particle.point })
                 fadeOutPoint = rotatePoint(fadeOutPoint, particle.getSpawnPoint(), .25)
                 particle.prepareFadeOut(fadeOutPoint)
             }
@@ -123,42 +154,76 @@ function ParticleImage(settings) {
 
     this.animate = function (transitionDataArr = null) {
 
+        const { timingFunction, frames } = this.animation
+
         this.cancelAnimation()
         let curFrame = 0
-        if (transitionDataArr) this.setParticleTransitions(transitionDataArr)
+        if (transitionDataArr) this.setParticles(transitionDataArr)
 
         const animateFrame = () => {
             this.canvas.clear()
-            let completion = curFrame / this.frames
-            let animationPosition = applyTimingFunction(completion, this.animationTimingFunction)
+            let completion = curFrame / frames
+            let animationPosition = applyTimingFunction(completion, timingFunction)
 
-            for (let i in this.particles) {
-                let particle = this.particles[i]
+            for (let i = 0; i < this.particles.array.length; i++) {
+                let particle = this.particles.array[i]
                 particle.setPropsToFramePosition(animationPosition)
                 particle.draw()
             }
             curFrame++
-            if (curFrame < this.frames) this._requestedFrame = requestAnimationFrame(animateFrame)
+            if (curFrame < frames) this._requestedFrame = requestAnimationFrame(animateFrame)
         }
 
         animateFrame()
     }
 
     this.getImageData = function (imageObj) {
-        return getImageData(imageObj, _this.width - _this.paddingX, _this.height - _this.paddingY)
+        const { width, height } = this.image.getContainedDimensions()
+        return getImageData(imageObj, width, height)
     }
+
+
+
+    this.getImageShift = function () {
+        //get x and y shift
+        const imageDimensions = this.image.getContainedDimensions()
+        const padding = this.canvas.padding
+
+        const canvasWidth = this.canvas.width
+        const canvasHeight = this.canvas.height
+        const imageWidth = imageDimensions.width
+        const imageHeight = imageDimensions.height
+
+        const { alignH, alignV } = this.image
+
+        let shiftX = 0,
+            shiftY = 0
+
+        if (alignH === 'left') shiftX = padding.left
+        else if (alignH === 'center') shiftX = (canvasWidth - imageWidth) / 2
+        else if (alignH === 'right') shiftX = canvasWidth - imageWidth - padding.right
+
+        if (alignV === 'top') shiftY = padding.top
+        else if (alignV === 'center') shiftY = (canvasHeight - imageHeight) / 2
+        else if (alignV === 'bottom') shiftY = canvasHeight - imageHeight - padding.bottom
+
+        return { shiftX: shiftX, shiftY: shiftY }
+    }
+
 
     this.getParticleDataFromImage = function (imageData, includeColor = true) {
         const { width, height, data } = imageData
-        let particleData = []
+        const { shiftX, shiftY } = this.getImageShift()
+        const { density } = this.particles
 
-        for (let x = 0; x < width; x += this.density) {
-            for (let y = 0; y < height; y += this.density) {
+        let particleData = []
+        for (let x = 0; x < width; x += density) {
+            for (let y = 0; y < height; y += density) {
                 let position = (x + y * width) * 4
                 if (data[position + 3] > 128) {
 
                     let transitionData = {
-                        toPoint: { x: x, y: y }
+                        toPoint: { x: x + shiftX, y: y + shiftY }
                     }
                     if (includeColor) {
                         let r = data[position],
@@ -181,20 +246,32 @@ function ParticleImage(settings) {
      * Gets a random point on canvas within the padding of the canvas
      * @returns {PointObject} random {x, y} 
      */
-    this.getRandomPoint = function (maxDistance = null, fromPoint = null) {
-        const constrainRange = (val, min, max) => Math.max(Math.min(val, max), min)
+    this.getRandomPoint = function (options = {}) {
 
-        if (maxDistance) {
-            fromPoint = isPointObject(fromPoint) ? fromPoint : { x: this.width / 2, y: this.height / 2 }
-            let x = fromPoint.x + getRandomFloat(maxDistance * -1, maxDistance)
-            let y = fromPoint.y + getRandomFloat(maxDistance * -1, maxDistance)
-            x = Math.round(constrainRange(x, 0, this.width - this.paddingX))
-            y = Math.round(constrainRange(y, 0, this.height - this.paddingY))
-            return { x: x, y: y }
+        const { width, height } = this.canvas
+        const padding = this.canvas.padding
+        const {
+            maxDistance = null,
+            fromPoint = null,
+            contain = this.animation.contain
+        } = options
+
+        let minX = contain ? padding.left : 0,
+            minY = contain ? padding.top : 0,
+            maxX = contain ? width - padding.right : width,
+            maxY = contain ? height - padding.bottom : height
+
+        if (typeof maxDistance === 'number') {
+            if (!isPointObject(fromPoint)) return void console.error('if setting a max distance, you must also specify a from point')
+            minX = Math.max(fromPoint.x - maxDistance, minX)
+            minY = Math.max(fromPoint.y - maxDistance, minY)
+            maxX = Math.min(fromPoint.x + maxDistance, maxX)
+            maxY = Math.min(fromPoint.y + maxDistance, maxY)
         }
+
         return {
-            x: Math.round(getRandomFloat(0, this.width - this.paddingX)),
-            y: Math.round(getRandomFloat(0, this.height - this.paddingY))
+            x: getRandomFloat(minX, maxX),
+            y: getRandomFloat(minY, maxY)
         }
     }
 
@@ -211,7 +288,7 @@ function ParticleImage(settings) {
     this.Particle = function (point = null, properties = null) {
 
         const spawnPoint = isPointObject(point) ? point : _this.getRandomPoint()
-        const spawnProperties = properties === null ? { ..._this.particleProperties } : properties
+        const spawnProperties = properties === null ? { ..._this.particles.properties } : properties
         const context = _this.canvas.context
 
         this.point = spawnPoint // { x: 30.43, y: 21.22 }
@@ -234,7 +311,7 @@ function ParticleImage(settings) {
             context.globalAlpha = this.properties.opacity
             context.fillStyle = this.properties.fill
             context.beginPath()
-            context.arc(this.point.x + _this.paddingX / 2, this.point.y + _this.paddingY / 2, this.properties.radius, 0, Math.PI * 2, false)
+            context.arc(this.point.x, this.point.y, this.properties.radius, 0, Math.PI * 2, false)
             context.fill()
             context.globalAlpha = 1
         }
@@ -243,6 +320,7 @@ function ParticleImage(settings) {
         //fires each frame to update properties throughout transition
         this.setPropsToFramePosition = function (completion) {
             const { fromPoint, fromProperties, toPoint, toProperties } = this.transitionData
+            const { moveFunction, propertyFunction } = _this.animation
 
             this.point = applyMoveFunction({
                 fromPoint: fromPoint,
@@ -250,7 +328,7 @@ function ParticleImage(settings) {
                 completion: completion,
                 randFloat: this.randFloat,
                 spawnPoint: spawnPoint
-            }, _this.animationMoveFunction)
+            }, moveFunction)
 
             // Get transition values for properties
             this.properties = applyPropertyFunction({
@@ -258,7 +336,7 @@ function ParticleImage(settings) {
                 toProperties: toProperties,
                 completion: completion,
                 randFloat: this.randFloat
-            }, _this.animationPropertyFunction)
+            }, propertyFunction)
         }
 
         this.prepareFadeOut = function (toPoint = null) {
@@ -283,10 +361,6 @@ function ParticleImage(settings) {
             })
         }
 
-        this.resetProperties = function () {
-            this.properties = _this.particleProperties
-        }
-
         this.updateTransitionData = function (transitionData = {}) {
             const { fromPoint = {}, fromProperties = {}, toPoint = {}, toProperties = {} } = transitionData
             this.transitionData.fromPoint = { ...this.transitionData.fromPoint, ...fromPoint }
@@ -298,7 +372,7 @@ function ParticleImage(settings) {
         this.prepareFadeIn(spawnPoint)
     }
 
-    this.canvas.init()
+
 
 
     // PRIVATE METHODS
@@ -315,7 +389,6 @@ function ParticleImage(settings) {
         const vRatio = canvas.height / image.height
         const ratio = Math.min(hRatio, vRatio);
 
-        //center image
         const centerShiftX = ((canvas.width - image.width * ratio) / 2)
         const centerShiftY = ((canvas.height - image.height * ratio) / 2)
 
@@ -356,7 +429,7 @@ function ParticleImage(settings) {
         const booster = 1 - (Math.abs(.5 * completion) * 2) // = 0 when completion = 0 or 1 and 1 when completion = .5.
 
         let betweenValues = []
-        Object.keys(_this.particleProperties).forEach(prop => {
+        Object.keys(_this.particles.properties).forEach(prop => {
             let transitionVal = getTransitionValue(fromProperties[prop], toProperties[prop], completion)
             betweenValues[prop] = transitionVal
         })
@@ -425,11 +498,7 @@ function ParticleImage(settings) {
      * True if is object containing keys x, y
      */
     function isPointObject(testVar) {
-        const isObject = 'object' === typeof testVar
-            && null !== testVar
-            && !Array.isArray(testVar)
-
-        if (!isObject) return false
+        if (!isObject(testVar)) return false
 
         const keys = Object.keys(testVar)
         return keys.length === 2 && keys.includes('x') && keys.includes('y')
@@ -472,6 +541,8 @@ function ParticleImage(settings) {
 
         return array;
     }
+
+
 
     /**
      * Returns color between two hex colors
@@ -526,11 +597,30 @@ function ParticleImage(settings) {
 
         return pointXY
     }
-    function getOrDefault(val, fallback) {
-        return typeof val === 'undefined' ? fallback : val
-    }
 
     function getRandomFloat(min, max) {
         return Math.random() * (max - min) + min
+    }
+
+    function isObject(item) {
+        return item && typeof item === 'object' && !Array.isArray(item)
+    }
+
+    function mergeDeep(target, ...sources) {
+
+        if (!sources.length) return target
+        const source = sources.shift()
+
+        if (isObject(target) && isObject(source)) {
+            for (const key in source) {
+                if (isObject(source[key]) && !(source[key] instanceof Element)) {
+                    if (!target[key]) Object.assign(target, { [key]: {} })
+                    mergeDeep(target[key], source[key])
+                } else {
+                    Object.assign(target, { [key]: source[key] })
+                }
+            }
+        }
+        return mergeDeep(target, ...sources)
     }
 }
